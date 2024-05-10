a4be47491f8b90cc8ac934e3457aad4a74808d63
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2024-05-09 22:03:28 -0300

    first commit

--- example/index.tsx
+++ example/index.tsx
@@ -1,13 +1,36 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { ProductCard, ProductTitle, ProductImage, ProductButtons } from '../.';
+
+const product = {
+  id: '1',
+  title: 'Coffee Mug - Card',
+  // img: './coffee-mug.png'
+}
 
 const App = () => {
   return (
-    <div>
-      <Thing />
-    </div>
+    <>
+      <ProductCard
+        product={product}
+        initialValues={{
+          count: 4,
+          maxCount: 10
+        }}
+      >
+        {
+          ({ reset, increaseBy, count, isMaxCountReached, maxCount }) => (
+            <div>
+              <ProductImage />
+              <ProductTitle />
+              <ProductButtons />
+            </div>
+          )
+        }
+
+      </ProductCard>
+    </>
   );
 };
 
